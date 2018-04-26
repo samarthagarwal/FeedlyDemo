23058f00ebfa63a7f7f5a2f39e42aefc67c557a7
Samarth Agarwal <samarthagarwal@live.com>
2018-04-26 23:01:04 +0530

    Removed Firebase Credentials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,12 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 var config = {
-  apiKey: "AIzaSyDNsGvlekmFCU6-qb6zway7wypZIR8Fym4",
-  authDomain: "feedly-a36e4.firebaseapp.com",
-  databaseURL: "https://feedly-a36e4.firebaseio.com",
-  projectId: "feedly-a36e4",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "179123632235"
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
